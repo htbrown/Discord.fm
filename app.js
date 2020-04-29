555fd2b66b06d8ee08f12c89f94d79b4c0e79609
Hayden <nedyah.brown@gmail.com>
2020-04-29 13:38:40 +0100

    🎉 v1.0

---
+++ app.js
@@ -0,0 +1,42 @@
+/*
+    [ Discord.fm ]
+    By Hayden Brown (htbrown.com)
+    Licensed under the MIT License.
+ */
+const config = require('./config'), DiscordRPC = require('discord-rpc'), LastFmNode = require('lastfm').LastFmNode, log = require('winston');
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+let l = new LastFmNode({
+    api_key: config.lastfm.key,
+    secret: config.lastfm.secret
+});
+
+let stream = l.stream(config.lastfm.username);
+
+stream.on('nowPlaying', (track) => {
+    rpc.setActivity({
+        details: `💿 ${track.name}`,
+        state: `👤 ${track.artist['#text']}`,
+        largeImageKey: config.icons.logo,
+        largeImageText: `🎵 ${track.album['#text']}`,
+        smallImageKey: config.icons.user,
+        smallImageText: `Discord.fm v${require('./package.json').version}`
+    })
+    log.info(`Now Playing: ${track.name} - ${track.artist['#text']}`);
+});
+
+stream.on('stoppedPlaying', (track) => {
+    rpc.clearActivity().then(r => log.info('Stopped.'));
+})
+
+stream.on('error', (error) => {
+    log.error(error.message);
+})
+
+rpc.on('ready', () => {
+    log.info('Listening for music.');
+    stream.start();
+})
+
+rpc.login({ clientId: config.discord.clientId }).catch(log.error);
