bcf33fb12b871bbb4ddae3841b3a6e80b799137b
Hayden Brown <nedyah.brown@gmail.com>
2020-06-18 10:28:21 +0100

    Using Journl instead of Winston

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
     By Hayden Brown (htbrown.com)
     Licensed under the MIT License.
  */
-const config = require('./config'), DiscordRPC = require('discord-rpc'), LastFmNode = require('lastfm').LastFmNode, log = require('winston');
+
+const config = require('./config'), 
+    DiscordRPC = require('discord-rpc'), 
+    LastFmNode = require('lastfm').LastFmNode, 
+    Journl = require('journl');
 
 const rpc = new DiscordRPC.Client({ transport: 'ipc' });
 
@@ -11,8 +15,9 @@ const l = new LastFmNode({
     api_key: config.lastfm.key,
     secret: config.lastfm.secret
 });
-
 const stream = l.stream(config.lastfm.username);
+
+const log = new Journl();
 
 stream.on('nowPlaying', (track) => {
     rpc.setActivity({
